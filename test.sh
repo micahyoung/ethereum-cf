052325ddfedeaa9fae33afc3c17b2c9239b39ee4
Micah Young <myoung@pivotal.io>
2017-07-13 11:20:30 -0400

    Improve logging for test

--- test.sh
+++ test.sh
@@ -3,39 +3,51 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
-echo "Miners peers"
-NUM=$(cf scale miners | grep instances | cut -d: -f2)
-let NUM--
-for i in $(seq 0 $NUM); do 
-  PEERS=$(cf ssh miners -i $i -c "app/geth attach --exec 'JSON.stringify(admin.peers)' app/data/geth.ipc" | jq 'fromjson | .[].network.remoteAddress')
-  echo $PEERS
-done
+echo "Miners peers (expect one):"
+cf ssh miners -i $i -c "app/geth attach --exec 'JSON.stringify(admin.peers)' app/data/geth.ipc" | jq 'fromjson | .[].network.remoteAddress'
+echo
 
-echo "Nodes peers"
-NUM=$(cf scale nodes | grep instances | cut -d: -f2)
-let NUM--
-for i in $(seq 0 $NUM); do 
-  PEERS=$(cf ssh nodes -i $i -c "app/geth attach --exec 'JSON.stringify(admin.peers)' app/data/geth.ipc" | jq 'fromjson | .[].network.remoteAddress')
-  echo $PEERS
-done
+echo "Nodes peers (expect one):"
+ssh nodes -i $i -c "app/geth attach --exec 'JSON.stringify(admin.peers)' app/data/geth.ipc" | jq 'fromjson | .[].network.remoteAddress'
+echo
 
-echo "Stopping miner"
+echo "Stopping miner (expect true):"
 cf ssh miners -i 0 -c "app/geth attach --exec 'miner.stop()' app/data/geth.ipc"
-echo "Balances: expect both 0 or greater"
-cf ssh nodes  -i 0 -c "app/geth attach --exec 'eth.accounts.map(function(account) {return web3.fromWei(eth.getBalance(account))});' app/data/geth.ipc"
-echo "Transaction Status: expect pending: 0, queued: 0"
+echo
+
+echo "Transaction Status (expect pending: 0, queued: 0):"
 cf ssh nodes  -i 0 -c "app/geth attach --exec 'txpool.status' app/data/geth.ipc"
-echo "Mining one block (may take 30s or more...)"
-cf ssh miners -i 0 -c "app/geth attach --exec 'miner.start(1); admin.sleepBlocks(1); miner.stop()' app/data/geth.ipc"
-echo "Balances: expect [+5, same]"
+echo
+
+echo "Balances (expect both 0 or greater):"
 cf ssh nodes  -i 0 -c "app/geth attach --exec 'eth.accounts.map(function(account) {return web3.fromWei(eth.getBalance(account))});' app/data/geth.ipc"
-echo Queuing transfer of 3 ether from first to second...
+echo
+
+echo "Mining one block - may take several minutes (expect true):"
+cf ssh miners -i 0 -c "app/geth attach --exec 'miner.start(1); admin.sleepBlocks(1); miner.stop()' app/data/geth.ipc"
+sleep 5 #wait for transactions to propigate on slow containers
+echo
+
+echo "Balances (expect [previous+5, same]):"
+cf ssh nodes  -i 0 -c "app/geth attach --exec 'eth.accounts.map(function(account) {return web3.fromWei(eth.getBalance(account))});' app/data/geth.ipc"
+echo
+
+echo "Queuing transfer of 3 ether from first to second (expect transaction id):"
 cf ssh nodes  -i 0 -c "app/geth attach --exec 'personal.unlockAccount(eth.accounts[0], \"password\"); eth.sendTransaction({from: eth.accounts[0], to: eth.accounts[1], value: web3.toWei(3, \"ether\")})' app/data/geth.ipc"
-echo "Transaction Status (should be pending: 1, queued: 0)"
-cf ssh nodes  -i 0 -c "app/geth attach --exec 'txpool.status' app/data/geth.ipc"
-echo "Mining one block (may take 30s or more...)"
+echo
+
+#echo "Transaction Status (expect pending: 1, queued: 0):"
+#cf ssh nodes  -i 0 -c "app/geth attach --exec 'txpool.status' app/data/geth.ipc"
+#echo
+
+echo "Mining one block - may take several minutes (expect true):"
 cf ssh miners -i 0 -c "app/geth attach --exec 'miner.start(1); admin.sleepBlocks(1); miner.stop()' app/data/geth.ipc"
-echo "Balances: expect [+2, +3]"
+echo
+
+echo "Balances (expect [previous+2, previous+3]):"
 cf ssh nodes  -i 0 -c "app/geth attach --exec 'eth.accounts.map(function(account) {return web3.fromWei(eth.getBalance(account))});' app/data/geth.ipc"
-echo "Transaction Status (should be pending: 0, queued: 0)"
+echo
+
+echo "Transaction Status (expect pending: 0, queued: 0):"
 cf ssh nodes  -i 0 -c "app/geth attach --exec 'txpool.status' app/data/geth.ipc"
+echo
