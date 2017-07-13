202efeb064b4974c5b6182022765a9f13b0711ef
Micah Young <myoung@pivotal.io>
2017-07-13 15:21:28 -0400

    Rename bootnode app name to bootnodes

--- up.sh
+++ up.sh
@@ -23,32 +23,32 @@ mkdir pcf-root/data
 cp -r keystore pcf-root/data/
 geth init --datadir="./pcf-root/data/" genesis.json
 
-cf delete bootnode -f
-cf delete miners   -f
-cf delete nodes    -f
-cf push bootnode -f manifests/bootnode-manifest.yml -p pcf-root/ --no-start
-cf push miners   -f manifests/miner-manifest.yml    -p pcf-root/ --no-start
-cf push nodes    -f manifests/node-manifest.yml     -p pcf-root/ --no-start
+cf delete bootnodes -f
+cf delete miners    -f
+cf delete nodes     -f
+cf push bootnodes -f manifests/bootnode-manifest.yml -p pcf-root/ --no-start
+cf push miners    -f manifests/miner-manifest.yml    -p pcf-root/ --no-start
+cf push nodes     -f manifests/node-manifest.yml     -p pcf-root/ --no-start
 
-cf start bootnode
+cf start bootnodes
 
-BOOTNODE_IP=$(cf ssh bootnode -c "hostname --ip-address")
+BOOTNODE_IP=$(cf ssh bootnodes -c "hostname --ip-address")
 cf set-env miners BOOTNODE_IP $BOOTNODE_IP
 cf set-env nodes BOOTNODE_IP $BOOTNODE_IP
 
 cf start miners
 cf start nodes
 
-cf allow-access miners nodes    --protocol udp --port 30301
-cf allow-access miners nodes    --protocol tcp --port 30301
-cf allow-access nodes  nodes    --protocol udp --port 30301
-cf allow-access nodes  nodes    --protocol tcp --port 30301
-cf allow-access nodes  miners   --protocol udp --port 30301
-cf allow-access nodes  miners   --protocol tcp --port 30301
-cf allow-access miners miners   --protocol udp --port 30301
-cf allow-access miners miners   --protocol tcp --port 30301
-cf allow-access nodes  bootnode --protocol udp --port 33445
-cf allow-access miners bootnode --protocol udp --port 33445
+cf allow-access miners nodes     --protocol udp --port 30301
+cf allow-access miners nodes     --protocol tcp --port 30301
+cf allow-access nodes  nodes     --protocol udp --port 30301
+cf allow-access nodes  nodes     --protocol tcp --port 30301
+cf allow-access nodes  miners    --protocol udp --port 30301
+cf allow-access nodes  miners    --protocol tcp --port 30301
+cf allow-access miners miners    --protocol udp --port 30301
+cf allow-access miners miners    --protocol tcp --port 30301
+cf allow-access nodes  bootnodes --protocol udp --port 33445
+cf allow-access miners bootnodes --protocol udp --port 33445
 
 echo "Mining two blocks to confirm DAG is created and cluster is up and settled. May take several minutes (expect true):"
 cf ssh miners -i 0 -c "app/geth attach --exec 'miner.start(1); admin.sleepBlocks(2); miner.stop()' app/data/geth.ipc"
