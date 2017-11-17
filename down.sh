1b1470e55e56eb4e103b61ce289f1722014d3c3b
Micah Young <myoung@pivotal.io>
2017-11-16 19:22:23 -0500

    Remove service as well

--- down.sh
+++ down.sh
@@ -7,6 +7,7 @@ read -p "This will delete all existing nodes and keys. Are you sure? " -n 1 -r
 echo
 [[ $REPLY =~ ^[Yy]$ ]] || exit 1
 
-cf delete bootnodes -f
-cf delete miners    -f
-cf delete nodes     -f
+cf delete -f bootnodes
+cf delete -f miners
+cf delete -f nodes
+cf delete-service -f ethereum-discovery
